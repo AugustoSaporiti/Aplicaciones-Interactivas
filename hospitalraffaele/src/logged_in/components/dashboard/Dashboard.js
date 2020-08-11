952cdf51eea4538c437619f4dbe4b4621fe5a2db
Augusto Saporiti <augusto.saporiti@philips.edu.ar>
2020-08-10 23:30:58 -0300

    cambio de visualizacion de pantalla

--- hospitalraffaele/src/logged_in/components/dashboard/Dashboard.js
+++ hospitalraffaele/src/logged_in/components/dashboard/Dashboard.js
@@ -1,48 +1,26 @@
-import React, { Fragment, useEffect } from "react";
+import { Box, Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
-import { Typography, Box } from "@material-ui/core";
+import React, { Fragment, useEffect } from "react";
 import SettingsArea from "./SettingsArea";
-import UserDataArea from "./UserDataArea";
-import AccountInformationArea from "./AccountInformationArea";
-import StatisticsArea from "./StatisticsArea";
 
 function Dashboard(props) {
   const {
     selectDashboard,
-    CardChart,
-    statistics,
-    toggleAccountActivation,
     pushMessageToSnackbar,
     targets,
     setTargets,
-    isAccountActivated,
   } = props;
 
   useEffect(selectDashboard, [selectDashboard]);
 
   return (
     <Fragment>
-      <StatisticsArea CardChart={CardChart} data={statistics} />
       <Box mt={4}>
         <Typography variant="subtitle1" gutterBottom>
-          Your Account
+          Bienvenido!
         </Typography>
       </Box>
-      <AccountInformationArea
-        isAccountActivated={isAccountActivated}
-        toggleAccountActivation={toggleAccountActivation}
-      />
-      <Box mt={4}>
-        <Typography variant="subtitle1" gutterBottom>
-          Settings
-        </Typography>
-      </Box>
-      <SettingsArea pushMessageToSnackbar={pushMessageToSnackbar} />
-      <UserDataArea
-        pushMessageToSnackbar={pushMessageToSnackbar}
-        targets={targets}
-        setTargets={setTargets}
-      />
+      <SettingsArea pushMessageToSnackbar={pushMessageToSnackbar}targets={targets} setTargets={setTargets} />
     </Fragment>
   );
 }
--- hospitalraffaele/src/logged_in/components/dashboard/SettingsArea.js
+++ hospitalraffaele/src/logged_in/components/dashboard/SettingsArea.js
@@ -2,13 +2,73 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Settings1 from "./Settings1";
 import Settings2 from "./Settings2";
+import { Tab, Tabs, Box, Typography } from "@material-ui/core"
+import UserDataArea from "./UserDataArea";
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 function SettingsArea(props) {
+
+  const {
+    targets,
+    setTargets,
+  } = props;
+
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
   const { pushMessageToSnackbar } = props;
   return (
     <Fragment>
-      <Settings1 pushMessageToSnackbar={pushMessageToSnackbar} />
-      <Settings2 pushMessageToSnackbar={pushMessageToSnackbar} />
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Item One" >
+
+        </Tab>
+        <Tab label="Item Two" >
+        </Tab>
+        <Tab label="Item Three" />
+      </Tabs>
+
+      <TabPanel value={value} index={0}>
+        <Settings1 pushMessageToSnackbar={pushMessageToSnackbar} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Settings2 pushMessageToSnackbar={pushMessageToSnackbar} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <UserDataArea
+          pushMessageToSnackbar={pushMessageToSnackbar}
+          targets={targets}
+          setTargets={setTargets}
+        />
+      </TabPanel>
+
     </Fragment>
   );
 }
