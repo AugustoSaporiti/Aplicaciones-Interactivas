bb6e7be15fa655c25390fa6708d8c0a4202251e9
Pau Gargan <gargan.p@gmail.com>
2020-08-27 05:48:55 -0300

    Agrego controller webservices con endpoints

--- hospitalraffaele/src/controllers/webServices.js
+++ hospitalraffaele/src/controllers/webServices.js
@@ -3,6 +3,10 @@ const rutaBack = "http://localhost:8080/";
 const urlWebServices = {
     listUsers: rutaBack + "api/user/list",
     createUsers: rutaBack + "api/user/create",
+    updateUsers: rutaBack + "api/user/update",
+    deleteUsers: rutaBack + "api/user/disable",
+    findUserByEmail: rutaBack + "api/user/find/email/",
+    listUsersByRole: rutaBack + "api/user/list/role/",
 }
 
 export default urlWebServices;
